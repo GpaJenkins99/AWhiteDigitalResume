179465cf0ae92aec4e976fb9e3f5b9c8c395467d
Adrian White Jr <111890078+GpaJenkins99@users.noreply.github.com>
2025-01-09 12:45:42 -0500

    Update app.py
    Update to resume

--- app.py
+++ app.py
@@ -81,9 +81,9 @@ st.write("#")
 st.subheader("Hard Skills")
 st.write(
     """
-     - Windows Server 2012/2016/2019,- TCP/IP, - WAN,    - Java,                    - System Administration  - Nessus
+     - Windows Server 2012-2022,- TCP/IP, - WAN,    - Java,                    - System Administration  - Nessus
      - Microsoft Office 365,    - DNS,    - VoIP,   - django,                  - Network Support        - Vcenter
-     - Cisco Router CLI,        - LAN,    - SCCM,   - Remote Access Software , - Vsphear                - Splunk
+     - AWS,                     - LAN,    - SCCM,   - Remote Access Software , - Vsphear                - Splunk
      - Adobe Creative Suite,    - VPN,    - WSUS,   - Microsoft Exchange,      - VCS                    - Service Now
      - Active Directory,        - DHCP,   - Python, - PowerShell,              - Vrealize               - Flexeara 
     """
@@ -116,6 +116,33 @@ st.write(
    - ► Responsible for hands-on installation, administration, and patching of RHEL/CentOS and UNIX operating systems.
    - ► Installed and maintained server infrastructure device operating system software, ensuring seamless functionality across platforms. both Windows and Linux
    - ► Conducted comprehensive scans of computer networks and Domains, diagnosing connectivity issues and addressing network vulnerabilities to uphold security standards.
+    """
+)
+# ----- JOB 1 -----
+st.write("🚧", "**Lead Systems Engineer/ Architect ODNI | CACI**")
+st.write("September 2023 to present")
+st.write("Tysons McLean VA")
+st.write(
+    """
+    (new contract leaving Innovative Reasoning)
+   - ► stablished and managed production, testing, and development domains, ensuring seamless operations.
+   - ► Implemented SCCM and orchestrated system updates to enhance performance and security.
+   - ► Managed Secure Internet Protocol Router (SIPR) accounts through active directory administration.
+   - ► Scheduled essential maintenance periods to minimize operational disruptions.
+   - ► OCollaborated with cross-functional teams to define, document, and align system requirements with business objectives.
+   - ► Possess extensive experience in Microsoft Systems Engineering, specializing in Windows platform operating systems (Windows 10 and Server 2016-2019).
+   - ► Proficient in Windows Active Directory suite, including DNS, DFS, ADCS, and GPOs.
+   - ► Demonstrated expertise in virtualization products such as VMware vSphere, ESXi, and vRealize Operations.
+   - ► Skilled in scripting languages, particularly PowerShell, for automation and task optimization.
+   - ► Developed Python scripts to streamline task sequences, enhancing operational efficiency.
+   - ► Designed and executed innovative network solutions, optimizing existing networks to meet organizational objectives.
+   - ► Provided robust support for network equipment, including routers, proxy servers, switches, WAN accelerators, DNS, and DHCP servers.
+   - ► Managed, configured, and operated Linux servers, integrating them for maintenance testing.
+   - ► Responsible for hands-on installation, administration, and patching of RHEL/CentOS and UNIX operating systems.
+   - ► Installed and maintained server infrastructure device operating system software, ensuring seamless functionality across platforms. both Windows and Linux
+   - ► Conducted comprehensive scans of computer networks and Domains, diagnosing connectivity issues and addressing network vulnerabilities to uphold security standards.
+   - ► Collaborated seamlessly with cross-functional teams to define, document, and align system requirements with business objectives.
+   - ► Worked closely with development teams to integrate Atlassian tools into workflows, providing guidance and support.
     """
 )
 
